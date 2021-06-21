26e88be70acf5786bd1c9d3a314fbb74d9f3467a
gaku.toyomasu <g.toyomasu@gmail.com>
2021-06-21 22:21:48 +0900

    report fix

--- src/models/Report.java
+++ src/models/Report.java
@@ -58,8 +58,8 @@ public class Report {
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name = "update_at", nullable = false)
-    private Timestamp update_at;
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
 
     public Integer getId() {
         return id;
@@ -109,12 +109,12 @@ public class Report {
         this.created_at = created_at;
     }
 
-    public Timestamp getUpdate_at() {
-        return update_at;
+    public Timestamp getUpdated_at() {
+        return updated_at;
     }
 
-    public void setUpdate_at(Timestamp update_at) {
-        this.update_at = update_at;
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
 
 
